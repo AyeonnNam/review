2adf2f9fae3e7da74e39b3bad291ed84fd4ee39c
name <boalove284@naver.com>
2022-07-19 15:20:05 +0900

    COMMIT

--- src/main/java/hi/core/AutoAppConfig.java
+++ src/main/java/hi/core/AutoAppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //basePackages = "hi.core",
+        excludeFilters = @ComponentScan.Filter
+                (type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
---
+++ src/test/java/hi/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,12 @@
+package hi.core.scan.filter;
+
+import org.junit.jupiter.api.Test;
+
+public class ComponentFilterAppConfigTest {
+    @Test
+    void filterScan(){
+
+    }
+
+    static class Compot
+}
